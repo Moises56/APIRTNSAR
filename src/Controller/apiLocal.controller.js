2cd40821c4b35429c206e4a3c414f71e7eeba72d
moises0814 <144181018+moises0814@users.noreply.github.com>
2024-01-22 16:15:53 -0600

    home

--- src/Controller/apiLocal.controller.js
+++ src/Controller/apiLocal.controller.js
@@ -1,29 +1,26 @@
 // controllers/apiController.js
-const ApiCtrl = {};
+const ApiLocalCtrl = {};
 
 import axios from 'axios';
 import { response } from 'express';
 
-ApiCtrl.consultaRTN = async (req, res) => {
-  const apiUrl = 'http://172.20.11.52:7075/int-middleware-gateway/api/v1/AMDC/ConsultaRTN'; // Reemplaza con la URL de tu API
-  const username = 'INT-AMDC-SW-API'; // Reemplaza con tu nombre de usuario
-  const password = 'X&#rjw1Z5S&2'; // Reemplaza con tu contraseña
+//Consulta por RTN
+ApiLocalCtrl.consultaRTNl = async (req, res) => {
+  const apiUrl = 'http://api.amdc.hn:3000/apirtn';
+  const username = 'INT-AMDC-SW-API'; 
+  const password = 'X&#rjw1Z5S&2';
   const { rtn } = req.body;
 
   try {
-    console.log({
-        apiUrl,
-        username,
-        password,
-        rtn
-
-    })
+    console.log('RTN: ', rtn)
     const response = await axios.post(apiUrl, { rtn }, {
       headers: {
-        'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+        // 'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
         'Content-Type': 'application/json'
       }
     });
+
+    
 
     res.json(response.data);
     console.log(response.data)
@@ -33,58 +30,57 @@ ApiCtrl.consultaRTN = async (req, res) => {
   }
 };
 
-
-// ApiCtrl.ventasBrutas = async (req, res) => {
-//     const apiUrl = 'http://172.20.11.52:7075/int-middleware-gateway/api/v1/AMDC/ConsultaVentasBrutas'; // Reemplaza con la URL de tu API
-//     const username = 'INT-AMDC-SW-API'; // Reemplaza con tu nombre de usuario
-//     const password = 'X&#rjw1Z5S&2'; // Reemplaza con tu contraseña
-//     const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
+// Consulta por Ventas Brutas
+ApiLocalCtrl.ventasBrutasl = async (req, res) => {
+    const apiUrl = 'http://api.amdc.hn:3000/ventaBruta'; // Reemplaza con la URL de tu API
+    const username = 'INT-AMDC-SW-API'; 
+    const password = 'X&#rjw1Z5S&2';
+    const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
   
-//     try {
-//       const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
-//         headers: {
-//           'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
-//           'Content-Type': 'application/json'
-//         }
-//       });
+    try {
+      console.log('Rtn: ', Rtn,PeriodoDesde,PeriodoHasta)
+      const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
+        headers: {
+        //   'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+          'Content-Type': 'application/json'
+        }
+      });
   
-//       res.json(response.data);
-//       console.log(response.data)
-//     } catch (error) {
-//       console.error('Error al consumir la API:', error);
-//       res.status(500).json({ error: 'Error al consumir la API' });
-//     }
-//   };
+      console.log('Esto viene: ', response.data)
+      res.json(response.data);
+    
+    } catch (error) {
+      // console.error('Error de RTN:', error);
+       // Prepara un objeto de error personalizado con la información específica
+       let customError = {
+        error: 'Error de RTN',
+        data: null,
+        isSuccess: false,
+        message: 'Error desconocido'
+      };
 
+      // Verifica si el error es una instancia de AxiosError
+      if (error instanceof axios.AxiosError) {
+        // Accede a las propiedades específicas del error de Axios
+        const axiosError = error;
 
-ApiCtrl.ventasBrutas = async (req, res) => {
-  const apiUrl = 'http://172.20.11.52:7075/int-middleware-gateway/api/v1/AMDC/ConsultaVentasBrutas';
-  const username = 'INT-AMDC-SW-API';
-  const password = 'X&#rjw1Z5S&2';
-  const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
+        // Accede a la respuesta (si está disponible)
+        const responseData = axiosError.response?.data;
 
-  try {
-    const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
-      headers: {
-        'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
-        'Content-Type': 'application/json'
+        if (responseData) {
+          customError = {
+            ...customError,
+            data: responseData.data,
+            isSuccess: responseData.isSuccess,
+            message: responseData.message
+          };
+        }
       }
-    });
+      console.log(customError)
 
-    // Verifica si la solicitud fue exitosa
-    if (response.data.isSuccess) {
-      const { data, isSuccess, message } = response.data;
-      
-      // Muestra la información deseada
-      res.json({ data, isSuccess, message });
-    } else {
-      // Si isSuccess es falso, significa que hubo un error en la solicitud
-      res.status(500).json({ error: message });
+      res.status(500).json(customError);
     }
-  } catch (error) {
-    console.error('Error al consumir la API:', error);
-    res.status(500).json({ error: 'Error al consumir la API' });
-  }
-};
+  };
 
-export default ApiCtrl;
+
+export default ApiLocalCtrl;
