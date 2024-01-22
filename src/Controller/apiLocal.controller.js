adf01d5dd13a581ec3bc244ba91d87f302066826
moises0814 <144181018+moises0814@users.noreply.github.com>
2024-01-22 16:10:11 -0600

    home

--- src/Controller/apiLocal.controller.js
+++ src/Controller/apiLocal.controller.js
@@ -1,82 +1,90 @@
 // controllers/apiController.js
-const ApiLocalCtrl = {};
+const ApiCtrl = {};
 
 import axios from 'axios';
 import { response } from 'express';
 
-//Consulta por RTN
-// ApiLocalCtrl.consultaRTNl = async (req, res) => {
-//   const apiUrl = 'http://api.amdc.hn:3000/apirtn';
-//   const { rtn } = req.body;
-
-//   try {
-//     console.log('RTN: ', rtn)
-//     const response = await axios.post(apiUrl, { rtn }, {
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
-
-    
-
-//     res.json(response.data);
-//     console.log(response.data)
-//   } catch (error) {
-//     console.error('Error al consumir la API:', error);
-//     res.status(500).json({ error: 'Error al consumir la API' });
-//   }
-// };
-
-
-// Consulta por RTN
-ApiLocalCtrl.consultaRTNl = async (req, res) => {
-  const apiUrl = 'http://api.amdc.hn:3000/apirtn';
+ApiCtrl.consultaRTN = async (req, res) => {
+  const apiUrl = 'http://172.20.11.52:7075/int-middleware-gateway/api/v1/AMDC/ConsultaRTN'; // Reemplaza con la URL de tu API
+  const username = 'INT-AMDC-SW-API'; // Reemplaza con tu nombre de usuario
+  const password = 'X&#rjw1Z5S&2'; // Reemplaza con tu contraseña
   const { rtn } = req.body;
 
   try {
-    console.log('RTN: ', rtn)
+    console.log({
+        apiUrl,
+        username,
+        password,
+        rtn
+
+    })
     const response = await axios.post(apiUrl, { rtn }, {
       headers: {
+        'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
         'Content-Type': 'application/json'
       }
     });
 
-    res.json({data: response.data});
+    res.json(response.data);
     console.log(response.data)
   } catch (error) {
-    console.log(error)
-    console.error('Error al consumir la API:', error.message);
-    res.status(500).json({ isSuccess: false, error: 'error al consultar el RTN ', errorMessage: error.message });
+    console.error('Error al consumir la API:', error);
+    res.status(500).json({ error: 'Error al consumir la API' });
   }
 };
 
 
-
-
-
-
-
-// Consulta por Ventas Brutas
-ApiLocalCtrl.ventasBrutasl = async (req, res) => {
-    const apiUrl = 'http://api.amdc.hn:3000/ventaBruta'; // Reemplaza con la URL de tu API
-    const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
+// ApiCtrl.ventasBrutas = async (req, res) => {
+//     const apiUrl = 'http://172.20.11.52:7075/int-middleware-gateway/api/v1/AMDC/ConsultaVentasBrutas'; // Reemplaza con la URL de tu API
+//     const username = 'INT-AMDC-SW-API'; // Reemplaza con tu nombre de usuario
+//     const password = 'X&#rjw1Z5S&2'; // Reemplaza con tu contraseña
+//     const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
   
-    try {
-      console.log('Rtn: ', Rtn,PeriodoDesde,PeriodoHasta)
-      const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+//     try {
+//       const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
+//         headers: {
+//           'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+//           'Content-Type': 'application/json'
+//         }
+//       });
   
-      console.log('Esto viene: ', response.data)
-      res.json(response.data);
-    
-    } catch (error) {
-      console.error('Error al consumir la API:', error);
-      res.status(500).json({ error: 'Error al consumir la API' });
+//       res.json(response.data);
+//       console.log(response.data)
+//     } catch (error) {
+//       console.error('Error al consumir la API:', error);
+//       res.status(500).json({ error: 'Error al consumir la API' });
+//     }
+//   };
+
+
+ApiCtrl.ventasBrutas = async (req, res) => {
+  const apiUrl = 'http://172.20.11.52:7075/int-middleware-gateway/api/v1/AMDC/ConsultaVentasBrutas';
+  const username = 'INT-AMDC-SW-API';
+  const password = 'X&#rjw1Z5S&2';
+  const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
+
+  try {
+    const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
+      headers: {
+        'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // Verifica si la solicitud fue exitosa
+    if (response.data.isSuccess) {
+      const { data, isSuccess, message } = response.data;
+      
+      // Muestra la información deseada
+      res.json({ data, isSuccess, message });
+    } else {
+      // Si isSuccess es falso, significa que hubo un error en la solicitud
+      res.status(500).json({ error: message });
     }
-  };
+  } catch (error) {
+    console.error('Error al consumir la API:', error);
+    res.status(500).json({ error: 'Error al consumir la API' });
+  }
+};
 
-
-export default ApiLocalCtrl;
+export default ApiCtrl;
