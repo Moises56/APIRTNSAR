aa8854e12f927b2eae7e233c2083f2938f763ed2
Moises Aviles <moiavil56@gmail.com>
2024-01-22 00:26:47 -0600

    erros

--- src/Controller/apiLocal.controller.js
+++ src/Controller/apiLocal.controller.js
@@ -5,55 +5,77 @@ import axios from 'axios';
 import { response } from 'express';
 
 //Consulta por RTN
+// ApiLocalCtrl.consultaRTNl = async (req, res) => {
+//   const apiUrl = 'http://api.amdc.hn:3000/apirtn';
+//   const { rtn } = req.body;
+
+//   try {
+//     console.log('RTN: ', rtn)
+//     const response = await axios.post(apiUrl, { rtn }, {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+
+    
+
+//     res.json(response.data);
+//     console.log(response.data)
+//   } catch (error) {
+//     console.error('Error al consumir la API:', error);
+//     res.status(500).json({ error: 'Error al consumir la API' });
+//   }
+// };
+
+
+// Consulta por RTN
 ApiLocalCtrl.consultaRTNl = async (req, res) => {
   const apiUrl = 'http://api.amdc.hn:3000/apirtn';
-  const username = 'INT-AMDC-SW-API'; 
-  const password = 'X&#rjw1Z5S&2';
   const { rtn } = req.body;
 
   try {
     console.log('RTN: ', rtn)
     const response = await axios.post(apiUrl, { rtn }, {
       headers: {
-        // 'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
         'Content-Type': 'application/json'
       }
     });
 
-    
-
-    res.json(response.data);
+    res.json({data: response.data});
     console.log(response.data)
   } catch (error) {
-    console.error('Error al consumir la API:', error);
-    res.status(500).json({ error: 'Error al consumir la API' });
+    console.log(error)
+    console.error('Error al consumir la API:', error.message);
+    res.status(500).json({ isSuccess: false, error: 'error al consultar el RTN ', errorMessage: error.message });
   }
 };
+
+
+
+
+
+
 
 // Consulta por Ventas Brutas
 ApiLocalCtrl.ventasBrutasl = async (req, res) => {
     const apiUrl = 'http://api.amdc.hn:3000/ventaBruta'; // Reemplaza con la URL de tu API
-    const username = 'INT-AMDC-SW-API'; 
-    const password = 'X&#rjw1Z5S&2';
     const { Rtn, PeriodoDesde, PeriodoHasta } = req.body;
   
     try {
       console.log('Rtn: ', Rtn,PeriodoDesde,PeriodoHasta)
       const response = await axios.post(apiUrl, { Rtn, PeriodoDesde, PeriodoHasta }, {
         headers: {
-        //   'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
           'Content-Type': 'application/json'
         }
-      });
-      response.data.forEach(element => {
-          console.log(element)
       });
   
       res.json(response.data);
       console.log('Esto viene: ', response.data)
     } catch (error) {
       console.error('Error al consumir la API:', error);
-      res.status(500).json({ error: 'Error al consumir la API' });
+      res.status(500).json({ isSuccess: false, error: 'Error al consultar el RTN', errorMessage: error.message, });
+
+      // res.status(500).json({ error: 'Error al consumir la API' });
     }
   };
 
